768590bac45e818476af513d34011098a26ad86d
PeiYun <64179604+Pattywu20031224@users.noreply.github.com>
2021-06-18 22:54:11 +0800

    Add files via upload

---
+++ H10610_飛機射擊/H10610_飛機射擊/Form1.Designer.vb
@@ -0,0 +1,209 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請勿使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox5 = New System.Windows.Forms.PictureBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer3 = New System.Windows.Forms.Timer(Me.components)
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.game_intro = New System.Windows.Forms.TextBox()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(561, 285)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(100, 109)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), System.Drawing.Image)
+        Me.PictureBox2.Location = New System.Drawing.Point(561, 12)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(100, 105)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox2.TabIndex = 1
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.Image = CType(resources.GetObject("PictureBox3.Image"), System.Drawing.Image)
+        Me.PictureBox3.Location = New System.Drawing.Point(597, 178)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(28, 48)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox3.TabIndex = 2
+        Me.PictureBox3.TabStop = False
+        Me.PictureBox3.Visible = False
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.Image = CType(resources.GetObject("PictureBox4.Image"), System.Drawing.Image)
+        Me.PictureBox4.Location = New System.Drawing.Point(410, 285)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(126, 109)
+        Me.PictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox4.TabIndex = 3
+        Me.PictureBox4.TabStop = False
+        Me.PictureBox4.Visible = False
+        '
+        'PictureBox5
+        '
+        Me.PictureBox5.Image = CType(resources.GetObject("PictureBox5.Image"), System.Drawing.Image)
+        Me.PictureBox5.Location = New System.Drawing.Point(685, 12)
+        Me.PictureBox5.Name = "PictureBox5"
+        Me.PictureBox5.Size = New System.Drawing.Size(103, 105)
+        Me.PictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox5.TabIndex = 4
+        Me.PictureBox5.TabStop = False
+        Me.PictureBox5.Visible = False
+        '
+        'Timer1
+        '
+        '
+        'Timer2
+        '
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("標楷體", 25.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Button1.Location = New System.Drawing.Point(271, 46)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(213, 71)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "遊戲開始"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("標楷體", 25.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Button2.Location = New System.Drawing.Point(271, 151)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(213, 75)
+        Me.Button2.TabIndex = 6
+        Me.Button2.Text = "遊戲說明"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'game_intro
+        '
+        Me.game_intro.Font = New System.Drawing.Font("標楷體", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.game_intro.Location = New System.Drawing.Point(116, 285)
+        Me.game_intro.Multiline = True
+        Me.game_intro.Name = "game_intro"
+        Me.game_intro.Size = New System.Drawing.Size(265, 109)
+        Me.game_intro.TabIndex = 7
+        Me.game_intro.Text = "按下左右鍵以幫助移動。"
+        Me.game_intro.Visible = False
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(1, 411)
+        Me.ProgressBar1.Maximum = 450
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(799, 39)
+        Me.ProgressBar1.TabIndex = 8
+        Me.ProgressBar1.Value = 450
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("標楷體", 25.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 12)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(108, 44)
+        Me.Label1.TabIndex = 9
+        Me.Label1.Text = "分數"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("標楷體", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Label2.Location = New System.Drawing.Point(109, 16)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(37, 40)
+        Me.Label2.TabIndex = 10
+        Me.Label2.Text = "0"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.game_intro)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.PictureBox5)
+        Me.Controls.Add(Me.PictureBox4)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents PictureBox4 As PictureBox
+    Friend WithEvents PictureBox5 As PictureBox
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Timer2 As Timer
+    Friend WithEvents Timer3 As Timer
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents game_intro As TextBox
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+End Class
---
+++ H10610_飛機射擊/H10610_飛機射擊/Form1.vb
@@ -0,0 +1,97 @@
+﻿Public Class Form1
+    Dim is_shoot As Boolean = True
+    Dim StartGame As Boolean = True
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+        If StartGame = True Then
+            game_intro.Visible = False
+            Button1.Visible = False
+            Button2.Visible = False
+            ProgressBar1.Visible = False
+        End If
+
+        '我方飛機移動
+        If e.KeyCode = Keys.Left Then
+            PictureBox1.Left -= 10
+        End If
+        If e.KeyCode = Keys.Right Then
+            PictureBox1.Left += 10
+        End If
+
+        '我方飛機穿牆
+        If PictureBox1.Left < 0 - PictureBox1.Width Then
+            PictureBox1.Left = Me.Width - 20
+        ElseIf PictureBox1.Left > Me.Width - 20 Then
+            PictureBox1.Left = 0 - PictureBox1.Width
+        End If
+
+        '我方子彈射擊
+        If e.KeyCode = Keys.Space Then
+            PictureBox3.Left = PictureBox1.Left + PictureBox1.Width / 2 - PictureBox3.Width / 2
+            PictureBox3.Visible = True
+            is_shoot = False
+            Timer1.Enabled = True
+        End If
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        PictureBox3.Visible = True
+        '我方子彈移動
+        PictureBox3.Top -= 40
+
+        '我方子彈打到敵機
+        If PictureBox3.Left < PictureBox2.Left + PictureBox2.Width And
+           PictureBox3.Left + PictureBox3.Width > PictureBox2.Left And
+           PictureBox3.Top < PictureBox2.Top + PictureBox2.Height And
+           PictureBox3.Top + PictureBox3.Height > PictureBox2.Top Then
+            My.Computer.Audio.Play(My.Application.Info.DirectoryPath & "\explosion2_1.wav", AudioPlayMode.Background)
+            PictureBox2.Image = PictureBox4.Image
+            Timer3.Enabled = False
+            Timer2.Enabled = True
+            Timer1.Enabled = False
+            PictureBox3.Top = PictureBox1.Top
+            PictureBox3.Visible = False
+        End If
+
+        '我方子彈出界
+        If PictureBox3.Top < 0 - PictureBox3.Height Then
+            Timer1.Enabled = False
+            PictureBox3.Top = PictureBox1.Top
+            PictureBox3.Visible = False
+            is_shoot = True
+        End If
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        Randomize()
+        Timer2.Enabled = False
+        Timer3.Enabled = True
+        PictureBox2.Image = PictureBox5.Image
+        PictureBox2.Left = Int(0 + Rnd() * (Me.Width - 20 - PictureBox2.Width - 0 + 1))
+        is_shoot = True
+    End Sub
+
+    Private Sub Timer3_Tick(sender As Object, e As EventArgs)
+        '敵機飛行
+        Randomize()
+        PictureBox2.Left = Int((PictureBox2.Left - 30) + Rnd() * ((PictureBox2.Left + 30) - (PictureBox2.Left - 30) + 1))
+
+        '敵機擋牆
+        If PictureBox2.Left < 0 Then
+            PictureBox2.Left = 0
+        ElseIf PictureBox2.Left > Me.Width - 20 - PictureBox2.Width Then
+            PictureBox2.Left = Me.Width - 20 - PictureBox2.Width
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        StartGame = True
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        game_intro.Visible = True
+    End Sub
+
+    Private Sub ProgressBar1_Click(sender As Object, e As EventArgs) Handles ProgressBar1.Click
+        ProgressBar1.Visible = True
+    End Sub
+End Class
